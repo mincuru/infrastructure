bf996121abc1a4d70d3834eed5c06076d205f7cf
awwa500@gmail.com <awwa500@gmail.com>
2023-01-09 16:51:01 +0900

    Initial commit

---
+++ src/Infrastructure/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace Infrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new InfrastructureStack(app, "InfrastructureStack");
+
+            app.Synth();
+        }
+    }
+}
